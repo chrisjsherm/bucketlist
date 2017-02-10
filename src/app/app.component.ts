47d6c52c3e9158e5aee9478a548bd3698a3f97d0
Chris Sherman <chris@cloudistics.com>
2017-02-10 18:32:47 -0500

    Basic todo list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  providers: [TodoDataService],
 })
 export class AppComponent {
-  title = 'app works!';
+
+    newTodo: Todo = new Todo();
+
+    constructor(private todoDataService: TodoDataService) {
+    }
+
+    addTodo() {
+        // Save the existing item.
+        this.todoDataService.addTodo(this.newTodo);
+
+        // Initialize a new object so we can add more items.
+        this.newTodo = new Todo();
+    }
+
+    get todos() {
+        return this.todoDataService.getAllTodos();
+    }
+
+    removeTodo(todo) {
+        this.todoDataService.deleteTodoById(todo.id);
+    }
+
+    toggleTodoComplete(todo) {
+        this.todoDataService.toggleTodoComplete(todo);
+    }
 }
