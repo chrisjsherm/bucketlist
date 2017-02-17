ccf0e36f2a8067c9a5b5bb6aa371e043ea7cc457
Chris Sherman <chris@cloudistics.com>
2017-02-17 17:39:37 -0500

    Add Foundation-Sites styling. Working on modifying todo input to a form.

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -4,64 +4,72 @@ import { Todo } from './todo';
 @Injectable()
 export class TodoDataService {
 
-  // Placeholder for last id so we can simulate automatic incrementing of IDs.
-  lastId: number = 0;
+    // Placeholder for last id so we can simulate automatic incrementing of IDs.
+    lastId: number = 0;
 
-  // Placeholder for todo's.
-  todos: Todo[] = [];
+    // Placeholder for todo's.
+    todos: Todo[] = [];
 
-  constructor() { }
-
-  // Simulate POST /todos.
-  addTodo(todo: Todo): TodoDataService {
-    if (!todo.id) {
-      todo.id = ++this.lastId;
+    constructor() {
+        this.addTodo(new Todo(
+            {
+                id: 1,
+                title: 'CLDTX-12314',
+                complete: false,
+            }
+        ))
     }
 
-    this.todos.push(todo);
+    // Simulate POST /todos.
+    addTodo(todo: Todo): TodoDataService {
+        if (!todo.id) {
+            todo.id = ++this.lastId;
+        }
 
-    return this;
-  }
+        this.todos.push(todo);
 
-  // Simulate DELETE /todos/:id.
-  deleteTodoById(id: number): TodoDataService {
-    this.todos = this.todos
-      .filter(todo => todo.id !== id);
-
-    return this;
-  }
-
-  // Simulate PUT /todos/:id.
-  updateTodoById(id: number, values: Object = {}): Todo {
-    let todo = this.getTodoById(id);
-
-    if (!todo) {
-      return null;
+        return this;
     }
 
-    Object.assign(todo, values);
+    // Simulate DELETE /todos/:id.
+    deleteTodoById(id: number): TodoDataService {
+        this.todos = this.todos
+            .filter(todo => todo.id !== id);
 
-    return todo;
-  }
+        return this;
+    }
 
-  // Simulate GET /todos.
-  getAllTodos(): Todo[] {
-    return this.todos;
-  }
+    // Simulate PUT /todos/:id.
+    updateTodoById(id: number, values: Object = {}): Todo {
+        let todo = this.getTodoById(id);
 
-  // Simulate GET /todos/:id.
-  getTodoById(id: number): Todo {
-    return this.todos
-      .filter(todo => todo.id === id)
-      .pop();
-  }
+        if (!todo) {
+            return null;
+        }
 
-  // Toggle todo complete.
-  toggleTodoComplete(todo: Todo) {
-    let updatedTodo = this.updateTodoById(todo.id, {
-      complete: !todo.complete
-    });
+        Object.assign(todo, values);
 
-    return updatedTodo;
-  }
+        return todo;
+    }
+
+    // Simulate GET /todos.
+    getAllTodos(): Todo[] {
+        return this.todos;
+    }
+
+    // Simulate GET /todos/:id.
+    getTodoById(id: number): Todo {
+        return this.todos
+            .filter(todo => todo.id === id)
+            .pop();
+    }
+
+    // Toggle todo complete.
+    toggleTodoComplete(todo: Todo) {
+        let updatedTodo = this.updateTodoById(todo.id, {
+            complete: !todo.complete
+        });
+
+        return updatedTodo;
+    }
 }
